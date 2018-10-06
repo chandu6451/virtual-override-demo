a94e3ea87eac688d58a0f32e6738f5e9ff22354f
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 12:52:15 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverrideDemo
+{
+    public class Employee
+    {
+        int salary;
+
+        public int Salary
+        {
+            get { return salary; }
+        }
+
+        public Employee(int sal)
+        {
+            salary = sal;
+        }
+
+        public virtual int GiveBonus(int amount)
+        {
+            return salary + amount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverrideDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee(20000);
+            Console.WriteLine("With Bonus Salary : " + emp.GiveBonus(5000));
+
+            SalesPerson sales = new SalesPerson(20000, 215);
+            Console.WriteLine("With Bonus Salary : " + sales.GiveBonus(5000));
+
+            Employee empSales = new SalesPerson(20000, 215);
+            Console.WriteLine("With Bonus Salary : " + empSales.GiveBonus(5000));
+
+            Console.ReadKey();
+        }
+    }
+}
